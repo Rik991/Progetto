730ba9b822e5970766142ccfdecfbe5339bc3bfa
Riccardo Santilli <riccardosantilli@live.it>
2024-10-25 17:32:43 +0200

    some details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   registerUrl: 'http://localhost:3000/register',
   loginUrl: 'http://localhost:3000/login',
+  moviesUrl: 'http://localhost:3000/movies-popular',
+  userUrl: 'http://localhost:3000/users',
+  favoritesUrl: 'http://localhost:3000/favorites',
 };
